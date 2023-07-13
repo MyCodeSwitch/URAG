518b9d7d5e825a0381103be9edb90e4410c0f96e
Your Name <you@example.com>
2023-07-13 17:08:33 -0400

    w!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-// Add custom JavaScript here
+let prevScrollPosition = window.scrollY || window.pageYOffset;
+
+window.addEventListener("scroll", function(){
+    let currentScrollPosition = window.scrollY || window.pageXOffset;
+
+    if (prevScrollPosition > currentScrollPosition){
+        document.querySelector(".navbar").classList.remove("navbar-hidden");
+    }else{
+        document.querySelector(".navbar").classList.add("navbar-hidden");
+    }
+
+    prevScrollPosition = currentScrollPosition;
+
+}
+)
