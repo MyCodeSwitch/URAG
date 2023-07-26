4e31c13de96d22ae959f31234b60aac57a435691
Jing Zhang <jing@planning.community>
2023-07-26 08:04:33 -0400

    726A

--- js/script.js
+++ js/script.js
@@ -91,31 +91,7 @@ const closeBtn = document.querySelector('#closeAlert');
 closeBtn.addEventListener('click', close);
 
 
-//Card Expand Button on Team page - HARD CODE
 
-let toggleBtn = document.querySelector('.toggleBtn');
-
-function memberInfoToggle (){
-    let toggleBtnContent = document.querySelector('.toggleBtnContent');
-    let toggleBtnIconPlus = document.querySelector('.toggleBtnIconPlus')
-    let toggleBtnIconMinus = document.querySelector('.toggleBtnIconMinus')
-
-    if (toggleBtnContent.classList.contains('myClose')){
-        toggleBtnContent.textContent = " ";
-        toggleBtnIconPlus.classList.add('d-none');
-        toggleBtnIconMinus.classList.remove('d-none');
-        toggleBtnContent.classList.remove('myClose');
-        toggleBtn.classList.remove('rounded-bottom')
-    }else{
-        toggleBtnContent.textContent = "More about Jing";
-        toggleBtnIconPlus.classList.remove('d-none');
-        toggleBtnIconMinus.classList.add('d-none');
-        toggleBtnContent.classList.add('myClose');
-        toggleBtn.classList.add('rounded-bottom')
-    }
-}
-
-toggleBtn.addEventListener('click', memberInfoToggle)
 
 //Card generation on team page - DYNAMIC
 
@@ -125,12 +101,98 @@ async function getData() {
     return content; 
   }
 
-getData().then((people)=>{
+getData()
+.then((people)=>{
     people.forEach(function (person){
+        //console.log(person);
+        const div = document.createElement('div');
+        div.classList.add('col-lg-4', 'col-md-6');
+        div.innerHTML = `
+        <div class="card m-3 pt-3 shadow align-self-stretch">
+        <!-- cardbody -->
+        <div class="card-body">
+          <div class="d-flex flex-column justify-content-center">
+              <div class="user-img d-flex justify-content-center">
+                <img src="https://randomuser.me/api/portraits/men/17.jpg" alt="" class="img-fluid rounded-circle" />
+              </div>
+                <div class="fs-4 fw-bold my-4 d-inline-flex justify-content-center"> <div>${person.first_name} ${person.last_name}, ${person.professional}</div></div>
+                <h6 class="mb-4 d-flex justify-content-center">${person.country}</h6>
+                <div class="d-flex justify-content-center mb-4">
+                  ${person.linkedIn ? 
+                      `<a id="linkedInIcon" href="https://www.cnn.com" target="_blank"><i class="fa-brands fa-linkedin fs-3"></i></a>
+                      ` : ``
+                  }
+                  ${person.email ? 
+                    `<a id="emailIcon" href="https://www.cnn.com" target="_blank"><i class="fa-solid fa-envelope fs-3 ms-3"></i></a>
+                    `:``
+                  }
+              
+                  
+                </div>
+                <div class="d-flex justify-content-center text-center">
+                  <p>${person.fields}</p> 
+                </div>
+          </div>
+        </div>
+
         
-        console.log(person)
+        <div class="expandBtn rounded-bottom d-flex justify-content-center align-items-center toggleBtn" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+          <div class="myClose px-3 toggleBtnContent unclickable-element inside-clickable">
+            More about ${person.first_name}
+          </div>
+          <i class="fa-solid fa-circle-plus p-3 ms-auto toggleBtnIconPlus unclickable-element inside-clickable"></i>
+          <i class="fa-solid fa-circle-minus p-3 ms-auto toggleBtnIconMinus unclickable-element inside-clickable d-none"></i>
+        </div>
+
+        <div class="container card-text expandCtn d-none">
+          <p class="p-3">
+            ${person.detail}
+          </p>
+        </div>
+      </div>
+        `;
+        document.querySelector('#personCtn').appendChild(div);
     })
 }
-
-
 ) 
+
+.then(function(){
+  let toggleBtns = document.querySelectorAll('.toggleBtn');
+  
+
+  toggleBtns.forEach((toggleBtn)=>{
+    console.log(toggleBtn);
+    toggleBtn.addEventListener('click', show);
+  })
+
+  function show(e){
+    let clickedElement = e.target;
+    
+    //click on the btn
+    if (e.target.classList.contains('expandBtn')){
+      let toggleBtn = clickedElement
+      let toggleBtnContent = clickedElement.querySelector('.toggleBtnContent');
+      let toggleBtnIconPlus = clickedElement.querySelector('.toggleBtnIconPlus');
+      let toggleBtnIconMinus = clickedElement.querySelector('.toggleBtnIconMinus');
+  
+      let moreInfo = clickedElement.nextElementSibling;
+  
+      if (toggleBtnContent.classList.contains('myClose')){
+        toggleBtnContent.textContent = "Close";
+        toggleBtnIconPlus.classList.add('d-none');
+        toggleBtnIconMinus.classList.remove('d-none');
+        toggleBtnContent.classList.remove('myClose');
+        toggleBtn.classList.remove('rounded-bottom')
+        moreInfo.classList.remove('d-none')
+       }else{
+        toggleBtnContent.textContent = "More about Jing";
+        toggleBtnIconPlus.classList.remove('d-none');
+        toggleBtnIconMinus.classList.add('d-none');
+        toggleBtnContent.classList.add('myClose');
+        toggleBtn.classList.add('rounded-bottom')
+        moreInfo.classList.add('d-none')
+    }}
+  }
+}
+)
+
--- team.php
+++ team.php
@@ -37,48 +37,8 @@ include 'inc/header.php';
 
 <!-- cards -->
 <div class="container mt-3 mb-7">
-  <div id="personCtn" class="row">
-    <div class="col-lg-4 col-md-6">
-      <div class="card m-3 pt-3 shadow">
-        <!-- cardbody -->
-        <div class="card-body">
-          <div class="d-flex flex-column justify-content-center">
-              <div class="user-img d-flex justify-content-center">
-                <img src="https://randomuser.me/api/portraits/men/17.jpg" alt="" class="img-fluid rounded-circle" />
-              </div>
-                <div class="fs-3 fw-bold my-4 d-inline-flex justify-content-center"> <div>Jing Zhang</div>,<div class="ms-2">AICP</div></div>
-                <h6 class="mb-4 d-flex justify-content-center">United States, China</h6>
-                <div class="d-flex justify-content-center">
-                  <a id="linkedInIcon" href="https://www.cnn.com" target="_blank"><i class="fa-brands fa-linkedin fs-3 me-3"></i></a>
-                  <a id="emailIcon" href="https://www.cnn.com" target="_blank"><i class="fa-solid fa-envelope fs-3 mb-4"></i></a>
-                </div>
-                <div class="d-flex justify-content-center text-center">
-                  <p>Transportation Planning, Full Stack Development</p> 
-                </div>
-          </div>
-        </div>
-
-        <!-- expand button -->
-        <div class="expandBtn rounded-bottom d-flex justify-content-center align-items-center toggleBtn" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-          <div class="myClose px-3 toggleBtnContent">
-            More about Jing
-          </div>
-          <i class="fa-solid fa-circle-plus p-3 ms-auto toggleBtnIconPlus"></i>
-          <i class="fa-solid fa-circle-minus p-3 ms-auto toggleBtnIconMinus d-none"></i>
-        </div>
-
-        <div class="container card-text collapse expandCtn" id="collapseExample">
-          <p class="p-3">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum voluptate commodi culpa porro tempora quasi minima,
-            impedit unde atque quia assumenda blanditiis dolore eius nobis eveniet doloribus reprehenderit laboriosam totam
-            natus earum eligendi. Perferendis voluptas nisi excepturi tempora modi quae dolores? Minus fugiat nesciunt ratione
-            corporis recusandae autem laudantium ullam libero, pariatur soluta, distinctio expedita dolores? Quae quisquam ratione
-            voluptatibus velit vero illo aliquam ut soluta nemo omnis saepe doloremque neque laboriosam alias tempore atque ex optio
-            quasi et sequi dolores rem, repellendus rerum nulla. Ab sit blanditiis eius laboriosam quae repellendus?
-          </p>
-        </div>
-      </div>
-    </div>
+  <div id="personCtn" class="row d-flex align-items-stretch">
+    
   </div>
 </div>
 
